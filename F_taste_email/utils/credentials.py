bc65b38b3ae1af48ca135872bf3d4e398733727e
EmmanuelZiviello <124155626+EmmanuelZiviello@users.noreply.github.com>
2025-03-24 19:47:34 +0100

    fixata mancanza di secret key per l'import

--- F_taste_email/utils/credentials.py
+++ F_taste_email/utils/credentials.py
@@ -5,3 +5,8 @@ mail_sender_password = os.environ.get('MAIL_SENDER_PASSWORD',"1&jc8EXLgd0xH&1Q")
 #port = 465
 port=2525# non uso ssl , di base sarebbe 465
 smtp_server = os.environ.get('SMTP_SERVER',"smtps.aruba.it")
+
+secret_key = os.environ.get('SECRET_KEY', b"\xf2\n\xdf\x07#\xd6J/ \x88\xa0\xb4'\x0f\xc7\x15")
+
+def get_key():
+    return os.environ.get('ENCRYPTION_KEY', 'Bcsoft!1')
