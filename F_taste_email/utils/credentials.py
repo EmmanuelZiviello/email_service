98339a1060467e32012f629a560c2fb1a216faca
EmmanuelZiviello <124155626+EmmanuelZiviello@users.noreply.github.com>
2025-03-24 18:51:50 +0100

    modifiche al smtp

--- F_taste_email/utils/credentials.py
+++ F_taste_email/utils/credentials.py
@@ -2,5 +2,6 @@ import os
 
 mail_sender_email = os.environ.get('MAIL_SENDER_EMAIL',"f-taste@airedstartup.it")
 mail_sender_password = os.environ.get('MAIL_SENDER_PASSWORD',"1&jc8EXLgd0xH&1Q")
-port = 465
+#port = 465
+port=2525# non uso ssl , di base sarebbe 465
 smtp_server = os.environ.get('SMTP_SERVER',"smtps.aruba.it")
